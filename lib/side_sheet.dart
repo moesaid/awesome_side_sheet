ab4665d36a34f6ef5f8f5c3e41f113f995ee463d
moesaid <masedup@gmail.com>
2023-12-18 16:33:59 -0500

    Update side_sheet.dart

--- lib/side_sheet.dart
+++ lib/side_sheet.dart
@@ -4,6 +4,50 @@ import 'package:awesome_side_sheet/Enums/sheet_position.dart';
 import 'package:awesome_side_sheet/build_side_sheet.dart';
 import 'package:flutter/material.dart';
 
+/// Displays a side sheet dialog with customizable options.
+///
+/// The [aweSideSheet] function displays a side sheet dialog with various customizable options such as the sheet position,
+/// widgets for the body, footer, and header, bool values to control the visibility of certain elements, strings for
+/// confirm and cancel action texts, duration for the transition animation, dimensions for the sheet width and border radius,
+/// colors for different UI elements, and callback functions for various events.
+///
+/// The [context] parameter is required and specifies the build context for the dialog.
+/// The [sheetPosition] parameter is required and specifies the position of the side sheet (either left or right).
+///
+/// The [body], [footer], and [header] parameters are optional and allow you to provide custom widgets for the corresponding sections of the side sheet.
+///
+/// The bool parameters such as [barrierDismissible], [showBackButton], [showCloseButton], [showActions], [showFooterDivider],
+/// [showHeaderDivider], [showSideDivider], [safeAreaTop], and [safeAreaBottom] control the visibility and behavior of certain elements in the side sheet.
+///
+/// The string parameters [confirmActionText], [cancelActionText], and [title] allow you to customize the text displayed in the side sheet.
+///
+/// The [transitionDuration] parameter allows you to specify the duration of the transition animation when showing or hiding the side sheet.
+///
+/// The [sheetWidth] parameter determines the width of the side sheet, while the [borderRadius] parameter allows you to specify the border radius of the side sheet.
+///
+/// The color parameters such as [backgroundColor], [surfaceTintColor], [textColor], [iconColor], [dividerColor], [confirmButtonBgColor],
+/// [confirmButtonTextColor], [cancelButtonBgColor], and [cancelButtonTextColor] allow you to customize the colors of different UI elements in the side sheet.
+///
+/// The callback parameters such as [onConfirm], [onCancel], [onDismiss], and [onClose] allow you to specify custom behavior for various events in the side sheet.
+///
+/// Example usage:
+/// ```dart
+/// aweSideSheet(
+///   context: context,
+///   sheetPosition: SheetPosition.right,
+///   body: Container(
+///     child: Text('This is the body of the side sheet'),
+///   ),
+///   title: 'Side Sheet',
+///   onConfirm: () {
+///     // Perform save action
+///   },
+///   onCancel: () {
+///     // Perform cancel action
+///   },
+/// );
+/// ```
+
 Future<void> aweSideSheet({
   // required
   required BuildContext context,
