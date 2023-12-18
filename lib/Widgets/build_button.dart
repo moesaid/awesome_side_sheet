2210bbaacfe94595ecf2384679e12acba705099c
moesaid <masedup@gmail.com>
2023-12-18 16:29:40 -0500

    added - BuildButton - doc

--- lib/Widgets/build_button.dart
+++ lib/Widgets/build_button.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+/// A button widget that can be used to build buttons with custom child widgets.
+///
+/// The [BuildButton] widget is a convenience widget that wraps the [ElevatedButton]
+/// widget and provides an optional [onPressed] callback and a required [child] widget.
+/// It can be used to create buttons with custom child widgets.
+///
+/// Example usage:
+///
+/// ```dart
+/// BuildButton(
+///   onPressed: () {
+///     // Perform some action when the button is pressed
+///   },
+///   child: Text('Click me'),
+/// )
+/// ```
+///
+
 class BuildButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final Widget child;
